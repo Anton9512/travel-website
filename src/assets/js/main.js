8baf7a8c4cf5275c5f88d208bb275dfe7f37c383
Anton9512 <Anton.mail9512@gmail.com>
2023-03-13 11:41:24 +0300

    add toggle menu in a header, 767px

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,3 +1,24 @@
-//= components/script.js
-//= components/script2.js
-//= components/script3.js
+/* SHOW MENU */
+
+const navMenu = document.getElementById("nav-menu");
+
+navToggle = document.getElementById("nav-toggle");
+navClose = document.getElementById("nav-close");
+
+if (navToggle) {
+  navToggle.addEventListener("click", function () {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+/*------------------*/
+
+/*---HIDDEN MENU---*/
+
+if (navClose) {
+  navClose.addEventListener("click", function () {
+    navMenu.classList.remove("show-menu");
+  });
+}
+
+/*------------------*/
