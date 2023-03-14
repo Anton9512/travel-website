cb5e2c25b8e468784eccf680e4ee1ccb489cdc6b
Anton9512 <Anton.mail9512@gmail.com>
2023-03-14 11:20:46 +0300

    home block, change background header on scroll

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,4 +1,4 @@
-/* SHOW MENU */
+/*-------SHOW MENU--------*/
 
 const navMenu = document.getElementById("nav-menu");
 
@@ -11,9 +11,9 @@ if (navToggle) {
   });
 }
 
-/*------------------*/
+/*--------------------------------------------------*/
 
-/*---HIDDEN MENU---*/
+/*HIDDEN MENU*/
 
 if (navClose) {
   navClose.addEventListener("click", function () {
@@ -21,4 +21,31 @@ if (navClose) {
   });
 }
 
-/*------------------*/
+/*---------------------------------------------------------*/
+
+/* Remove nav-menu on link click */
+const navLink = document.querySelectorAll(".nav__link");
+
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+
+  navMenu.classList.remove("show-menu");
+}
+
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+/*--------------------------------------------------------*/
+
+/* CHANGE BACKGROUND HEADER */
+
+function scrollHeader() {
+  const header = document.getElementById("header");
+
+  //When the scroll is greater than 100 viewport height, add the scroll header class to the header tag
+  if (this.scrollY >= 100) header.classList.add("scroll-header");
+  else header.classList.remove("scroll-header");
+}
+
+window.addEventListener("scroll", scrollHeader);
+
+/*------------------------------------------------------- */
